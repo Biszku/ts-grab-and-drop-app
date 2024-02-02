07f65377e6e6abe2bde4a5b0cc7f5a9bf010ce4a
Biszku <dbichu@onet.pl>
2024-02-02 13:52:21 +0100

    little change

--- src/ts/view/view.ts
+++ src/ts/view/view.ts
@@ -3,6 +3,7 @@ import { TaskData } from "../types";
 class View {
   tasksState: TaskData[] = [];
   container = document.querySelector("#container") as HTMLDivElement;
+
   render(element: Element) {
     this.container.insertAdjacentElement("afterbegin", element);
   }
